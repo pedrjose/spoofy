fd4bb9ebae9a41fceb53337544e160fc0e9b6ae4
eduardo-alves-neto <emailforprojects01@gmail.com>
2024-10-01 07:58:07 -0300

    feat: add favicon

--- src/components/cardSong/index.tsx
+++ src/components/cardSong/index.tsx
@@ -9,15 +9,17 @@ export const CardSong = ({ songs }: ICardSong) => {
     <>
       {songs.map((song) => (
         <div
-          key={song.name}
-          className={`w-[260px] bg-[#1E293B] rounded-lg p-3 transition-transform duration-300 ease-in-out transform hover:scale-[1.02]`}
+          key={song.name} //TODO: mudar key para p ID da musica
+          className={`w-[260px] bg-[#232d3df4] rounded-lg p-3 transition-transform duration-300 ease-in-out transform hover:scale-[1.02]`}
         >
           <div className="w-full h-40 bg-[#0F172A] rounded-md mb-1 flex items-center justify-center">
             <Music className="w-10 h-10 text-[#4ADE80]" />
           </div>
 
-          <h3 className="font-semibold text-lg">{song.name}</h3>
-          <p className="text-sm text-gray-400">{song.artist}</p>
+          <div className="py-1">
+            <h3 className="font-semibold text-lg">{song.name}</h3>
+            <p className="text-sm text-gray-400">{song.artist}</p>
+          </div>
 
           {song?.unlocked ? (
             <button className="w-full bg-[#4ADE80] text-[#0F172A] font-semibold py-2 rounded-md hover:bg-green-400 transition-colors duration-300 animate-pulse">
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -37,24 +37,26 @@ const songs = [
   { id: 1, name: "Stairway to Heaven", artist: "Led Zeppelin", unlocked: true },
   { id: 1, name: "Stairway to Heaven", artist: "Led Zeppelin", unlocked: true },
 ];
+
 export const Home = () => {
   return (
     <div className="flex h-full gap-2 ">
-      <aside className="w-80 bg-[#2c3444] hidden sm:block text-white p-4 rounded-lg">
+      <aside className="w-80 bg-[#2c3444] hidden md:block  text-white p-4 rounded-3xl">
         <h2 className="text-lg font-bold">mwdioeiwdoiw</h2>
       </aside>
 
-      <main className="flex-1 bg-[#2c3444] p-2 rounded-lg overflow-y-auto max-h-[calc(100vh-64px)] scrollbar-thin scrollbar-thumb-[#4ade8086] scrollbar-track-[#2c3444]">
-        <div className="my-7 flex flex-row justify-between pr-5">
+      <main className="flex-1 bg-[#56595e30] p-2 rounded-3xl overflow-y-auto max-h-[calc(100vh-64px)] scrollbar-thin scrollbar-thumb-[#4ade8086] scrollbar-track-[#2c3444] mr-1">
+        <div className="my-7 flex flex-row justify-between items-center pr-1">
           <h1 className="text-2xl font-bold  mb-4 uppercase tracking-wide">
             {"ESCOLHA SUA PRÓXIMA MÚSICA"}
           </h1>
+
           <button>
             <ListFilter className="w-[100px] " />
           </button>
         </div>
 
-        <div className="flex flex-row flex-wrap gap-6">
+        <div className="flex flex-row flex-wrap gap-6 md:justify-start justify-center">
           <CardSong songs={songs} />
         </div>
       </main>

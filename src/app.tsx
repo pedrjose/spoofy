056652e68e6553f5eb2c41a676054032b92a0f99
José Vitor <vitor200450@gmail.com>
2024-11-06 19:21:09 -0300

    style: fix autofill style on inputs

--- src/app.tsx
+++ src/app.tsx
@@ -1,34 +1,34 @@
-import { Login } from "./auth/login";
-import { Register } from "./auth/register";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { Route, Routes } from "react-router";
-import "react-toastify/dist/ReactToastify.css";
-import { ToastContainer } from "react-toastify";
 import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Login } from "./auth/login";
+import PrivateRoute from "./auth/privateRouter";
+import { Register } from "./auth/register";
 import { queryClient } from "./constants/queryClient";
 import { AuthProvider } from "./context/auth/AuthContext";
-import { QueryClientProvider } from "@tanstack/react-query";
 import { Home } from "./pages/home";
-import PrivateRoute from "./auth/privateRouter";
 import {} from "./pages/learningPage";
 
 function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+    return (
+        <QueryClientProvider client={queryClient}>
+            <AuthProvider>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Login />} />
+                        <Route path="/register" element={<Register />} />
 
-            <Route element={<PrivateRoute />}>
-              <Route path="/home" element={<Home />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-        <ToastContainer />
-      </AuthProvider>
-    </QueryClientProvider>
-  );
+                        <Route element={<PrivateRoute />}>
+                            <Route path="/home" element={<Home />} />
+                        </Route>
+                    </Routes>
+                </BrowserRouter>
+                <ToastContainer />
+            </AuthProvider>
+        </QueryClientProvider>
+    );
 }
 
 export default App;
